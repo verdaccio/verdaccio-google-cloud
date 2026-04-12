5654d2f144d92f247c88b9d7d1569a656afb9a1a
Juan Picado <juanpicado19@gmail.com>
2026-04-12 12:39:01 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import verdaccio from '@verdaccio/eslint-config';
 
 export default [
   {
-    ignores: ['generators/**', 'src/app/templates/**', 'docs/**', '**/dist/**', '**/node_modules/**', 'new_structure/**', 'coverage/**', '*.config.ts', '*.config.mjs'],
+    ignores: ['generators/**', 'src/app/templates/**', 'docs/**', '**/dist/**', '**/node_modules/**', 'new_structure/**', 'coverage/**', 'cypress/**', '*.config.ts', '*.config.mjs'],
   },
   ...verdaccio,
 ];
